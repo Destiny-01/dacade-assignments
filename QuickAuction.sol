ac245527fd3306d71d96d59b68a6a2a3d7e193ed
Farzeen Kistnareddy <90398313+FarzeenKist@users.noreply.github.com>
2022-06-10 19:45:53 +0400

    Fixed some typos, added more security and refactored redundant code

--- QuickAuction.sol
+++ QuickAuction.sol
@@ -18,7 +18,6 @@ contract QuickAuction is ERC721URIStorage {
         uint256 startPrice;
         uint256 highestBid;
         address highestBidder;
-        address[] buyers;
         uint256 endTime;
         bool isActive;
         bool ownerTaken;
@@ -26,6 +25,7 @@ contract QuickAuction is ERC721URIStorage {
 
     mapping (address => mapping (uint256 => uint256)) bids;
     mapping (uint256 => Auction) auctions;
+    mapping(uint256 => bool) tokenAuctioned;
 
     event NewAuction(uint256 id);
     event Bid(address indexed sender, uint256 amount);
@@ -39,14 +39,13 @@ contract QuickAuction is ERC721URIStorage {
         _;
     }
     
-    /* Mints the token to them */
+    /* Mints token to caller */
     function mintToken(string memory tokenURI)
         public
         returns (uint256)
     {
-        _tokenIds.increment();
-
         uint256 newItemId = _tokenIds.current();
+        _tokenIds.increment();
         _mint(msg.sender, newItemId);
         _setTokenURI(newItemId, tokenURI);
 
@@ -65,6 +64,9 @@ contract QuickAuction is ERC721URIStorage {
         uint256 _startPrice,
         uint256 _endTime
     ) external {
+        require(_tokenId >= 0, "Enter a valid tokenId");
+        require(bytes(_title).length > 0, "Enter a valid title");
+        require(!tokenAuctioned[_tokenId], "Nft is already being auctioned!");
         uint256 id = uint256(keccak256(bytes(_title))) % block.timestamp;
         
         auctions[id].id = id;
@@ -75,6 +77,7 @@ contract QuickAuction is ERC721URIStorage {
         auctions[id].owner = msg.sender;
         auctions[id].isActive = true;
 
+        tokenAuctioned[_tokenId] = true;
         _transfer(msg.sender, address(this), _tokenId);
         emit NewAuction(id);
     }
@@ -83,7 +86,6 @@ contract QuickAuction is ERC721URIStorage {
     function bid (uint256 _id) payable external isTimeUp(_id) {
         require(block.timestamp < auctions[_id].endTime || auctions[_id].isActive, "Auction ended");
         require(msg.sender != auctions[_id].owner, "Owner can't bid duh!!!");
-        require(msg.value > 0, "Send something duh!!!");
         require(msg.value > auctions[_id].startPrice, "Amount is less than start price");
 
         uint256 currentBid = bids[msg.sender][_id] + msg.value;
@@ -91,21 +93,17 @@ contract QuickAuction is ERC721URIStorage {
         if (msg.value > auctions[_id].highestBid && msg.value > auctions[_id].startPrice) {
             auctions[_id].highestBid = msg.value;
         } else {
-            require(currentBid > auctions[_id].startPrice, "Amount is less than start price");
             require(currentBid > auctions[_id].highestBid, "Amount is less than current bid");
             auctions[_id].highestBid = currentBid;
         }
 
         auctions[_id].highestBidder = msg.sender;
-        if (bids[msg.sender][_id] == 0) {
-            auctions[_id].buyers.push(msg.sender);
-        }
         bids[msg.sender][_id] = currentBid;
 
         emit Bid(msg.sender, currentBid);
     }
 
-    /* Auction is over, collect your rewards */
+    /* When auction is over, collect your rewards */
     function timeUp (uint256 _id) external isTimeUp(_id) {
         require(!auctions[_id].isActive, "Auction has not yet ended");
         require(block.timestamp > auctions[_id].endTime, "Auction has not expired");
@@ -114,24 +112,27 @@ contract QuickAuction is ERC721URIStorage {
             if (auctions[_id].highestBidder == address(0)) {
                 _transfer(address(this), msg.sender, auctions[_id].tokenId);
             } else {
-                payable(auctions[_id].owner).transfer(auctions[_id].highestBid);
+               (bool success,) = payable(auctions[_id].owner).call{value : auctions[_id].highestBid}("");
+               require(success, "Transfer of highest bid to owner failed");
             }
             auctions[_id].ownerTaken = true;
         } else if (auctions[_id].highestBidder == msg.sender) {
             _transfer(address(this), auctions[_id].highestBidder, auctions[_id].tokenId);
         } else {
             require(bids[msg.sender][_id] != 0, "Did you participate???");
-            payable(msg.sender).transfer(bids[msg.sender][_id]);
+            (bool success,)  = payable(msg.sender).call{value : bids[msg.sender][_id]}("");
+            require(success, "Refund failed");
         }
         bids[msg.sender][_id] = 0;
+        tokenAuctioned[auctions[_id].tokenId] = false;
     }
 
-    /* Get details of the auction with that id */
+    /* Gets details of the auction with that id */
     function getAuction(uint256 _id) external view returns (Auction memory auction) {
         return auctions[_id];
     }
 
-    /* Get how long is remaining to auction for a specific auction */
+    /* Gets how much time remains for a specific auction */
     function getTimeRemaining(uint256 _id) external view returns (uint256 time) {
         if(auctions[_id].endTime > block.timestamp) {
             return auctions[_id].endTime - block.timestamp;
