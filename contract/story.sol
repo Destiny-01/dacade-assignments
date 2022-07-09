ac5b6016aa2a2c3144f77b7b69f23a187a8fedc8
Suleiman Ibrahim <suleimanonnet@gmail.com>
2022-07-09 16:09:26 +0100

    Optimised contract

--- contract/story.sol
+++ contract/story.sol
@@ -7,11 +7,13 @@ contract StoryBook {
         string description;
         string status;
         address owner;
-        uint256 createdAt;
+        uint256 createdAt;  
     }
 
-    mapping (address => mapping (uint256 => Story)) myStories;
-    Story[] stories;
+    mapping(uint256 => Story) private stories;
+    mapping(address => uint256) private privateStoriesCount;
+    uint256 private publicStoriesCount;
+    uint256 private storiesIndex;
 
     event NewStory(uint256 id, address owner);
     event DeletedStory(uint256 _id);
@@ -20,49 +22,76 @@ contract StoryBook {
       ║        STORY FUNCTIONS      ║
       ╚═════════════════════════════╝*/
 
-    /* Creates a new story provided the right parameters are given */
-    function createStory(
+    function compareStrings(string memory a, string memory b) public pure returns (bool) {
+        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
+    }
+
+    /* Creates a new story provided the right parameters are given */ 
+    function createStory( 
         uint256 _id,
         string memory _title,
         string memory _description,
         string memory _status
     ) external {
-        require(myStories[msg.sender][_id].createdAt == 0, "Id taken");
-        myStories[msg.sender][_id].title = _title;
-        myStories[msg.sender][_id].description = _description;
-        myStories[msg.sender][_id].status = _status;
-        myStories[msg.sender][_id].owner = msg.sender;
-        myStories[msg.sender][_id].createdAt = block.timestamp;
+        stories[storiesIndex++] = Story(
+            _title,
+            _description,
+            _status, 
+            msg.sender,
+            block.timestamp
+        );
 
-        stories.push(Story(_title, _description, _status, msg.sender, block.timestamp));
-
+        if (compareStrings(_status, "private")) {
+            privateStoriesCount[msg.sender]++;
+        } else if (compareStrings(_status, "public")) {
+            publicStoriesCount++;
+        }
         emit NewStory(_id, msg.sender);
     }
 
-    /* Deletes a story provided you are the owner */
-    function deleteStory(uint256 _id) external {
-        for (uint256 i = 0; i < stories.length; i++){
-            if (stories[i].createdAt == _id && stories[i].owner == msg.sender){
-                delete(myStories[msg.sender][_id]);
-                delete(stories[i]);
-                emit DeletedStory(_id);
-                return;
+    // return all my private stories
+    function getMyPrivateStories() public view returns (Story[] memory) {
+        Story[] memory allPrivateStories = new Story[](privateStoriesCount[msg.sender]);
+        for (uint256 i = 0; i < storiesIndex; i++) {
+            if (stories[i].owner == msg.sender) {
+                allPrivateStories[i] = stories[i];
             }
         }
-        revert("Story not found or not yours");
+        return allPrivateStories;
     }
 
-    /* Get details of the story with that id */
-    function getStory(uint256 _id) external view returns (Story memory story) {
-        for (uint256 i = 0; i < stories.length; i++){
-            if (stories[i].createdAt == _id){
-                return stories[i];
-            }
+    // get all public stories
+    function getAllPublicStories() public view returns (Story[] memory) {        
+        Story[] memory allPublicStories = new Story[](publicStoriesCount);
+        for (uint256 i = 0; i < storiesIndex; i++) {
+            if (compareStrings(stories[i].status, "public")) {
+                allPublicStories[i] = stories[i];
+            }            
         }
+        return allPublicStories;
     }
 
-    /* Get all stories */
-    function getStories() external view returns (Story[] memory story) {
-        return stories;
+    // delete story at index @_index
+    function deleteStory(uint256 _index) public {
+        require(stories[_index].owner == msg.sender, "Only owner can delete story");
+        string memory status = stories[_index].status;
+        stories[_index] = stories[storiesIndex - 1];
+        delete stories[storiesIndex - 1];  
+
+        if (compareStrings(status, "private")) {
+            privateStoriesCount[msg.sender]--;
+        } else if (compareStrings(status, "public")) {
+            publicStoriesCount--;
+        }      
+    }
+
+    // return public stories length
+    function getStoriesPublicLength() external view returns (uint256) {
+        return publicStoriesCount;
+    }
+
+    // return private stories length
+    function getPrivateStoriesLength() public view returns (uint256) {
+        return privateStoriesCount[msg.sender];
     }
 }
